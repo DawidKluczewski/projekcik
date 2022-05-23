70e2430e875a5f18460fc26bcccc21556242cf7f
siemowit <IDawid923@gmail.com>
2022-05-23 02:17:55 +0200

    wszystko dziala :>

--- router2.js
+++ router2.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const router = express.Router();
-const service = require('./service2');
+const router2 = express.Router();
+const service2 = require('./service2');
 
-
-
-
-router.get( '/', async function(req, res, next ) {
+router2.get( '/', async function(req, res, next ) {
     try {
-        res.json(await service.wypozyczenia.all());
+        res.json(await service2.wypozyczenia.all());
     }catch(err){
         console.error('No sorry, wypozyczen nie mamy', err.message);
         next(err);
@@ -15,40 +12,40 @@ router.get( '/', async function(req, res, next ) {
     }
 );
 
-router.get('/:idwypozyczenia', async function(req, res, next){
+router2.get('/:idwypozyczenia', async function(req, res, next){
         try {
-            res.json(await service.wypozyczenia.single(req.params.idwypozyczenia));
+            res.json(await service2.wypozyczenia.single(req.params.idwypozyczenia));
         } catch(err) {
             console.error('Nie ma wypozyczenia', err.message);
             next(err);
     }
 });
 
-router.post('/', async function(req, res, next){
+router2.post('/', async function(req, res, next){
     try {
-        res.json(await service.wypozyczenia.create(req.body));
+        res.json(await service2.wypozyczenia.create(req.body));
     } catch (err){
         console.error('Nie udało się dopisać wypozyczenia', err.message);
         next(err);
     }
 });
 
-router.put('/:idwypozyczenia', async function(req, res, next){
+router2.put('/:idwypozyczenia', async function(req, res, next){
     try {
-        res.json(await service.wypozyczenia.update(req.params.idwypozyczenia, req.body));
+        res.json(await service2.wypozyczenia.update(req.params.idwypozyczenia, req.body));
     } catch (err){
         console.error('Nie udało się zmienić wypozyczenia', err.message);
         next(err);
     }
 });
 
-router.delete('/:idwypozyczenia', async function(req, res, next){
+router2.delete('/:idwypozyczenia', async function(req, res, next){
     try{
-        res.json(await service.wypozyczenia.delete(req.params.idwypozyczenia));
+        res.json(await service2.wypozyczenia.delete(req.params.idwypozyczenia));
     }catch(err){
         console.error('Nie usunę wypozyczenia', err.message);
         next(err);
     }
 });
 
-module.exports = router;
+module.exports = router2;
